3fab7fc7049d8e47c12577439382bd0db8426e0c
guerinsineadneuro <sguerin@neurobuildingsystems.com>
2026-04-07 18:14:55 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/neuro-annual-fee-calculator/',
+  base: '/neurocore-annual-fee-calculator/',
 })
